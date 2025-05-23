11e37847354a27c44bb4fe7af5bf1ce8e7a74ab5
Manu-Madhu <manum66466@gmail.com>
2025-05-23 20:29:55 +0530

    update

--- src/service/seohelper.js
+++ src/service/seohelper.js
@@ -3,7 +3,9 @@ import { getSeoData } from "@/service/api";
 export async function buildSeoMetadata(page) {
   const seo = await getSeoData(page);
   const url = `https://www.sarvamsafety.com/${page === "home" ? "" : page}`;
-  const image = seo.img || 'https://api.sarvamsafety.com/site/Artboard 3.8.jpg'
+
+  // update the for preview products
+  const image = seo.img || '/assets/favicon.png'
   return {
     title: seo.title,
     description: seo.description,
